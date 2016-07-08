ac3d0b1d9d93e23845949f664c277b92de2fcfa2
Ján Bočínec <jan.bocinec@webikon.sk>
2016-07-08 17:50:38 +0200

    ini commit

---
+++ src/TemplateInstallerPlugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Webikon\Composer\Installer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class TemplateInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new TemplateInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
