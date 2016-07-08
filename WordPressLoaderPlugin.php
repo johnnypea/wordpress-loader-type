db16be2d6457d5ed623f87fdf05234cc453a895d
Ján Bočínec <jan.bocinec@webikon.sk>
2016-07-08 20:31:35 +0200

    final testing

--- WordPressLoaderPlugin.php
+++ WordPressLoaderPlugin.php
@@ -12,6 +12,7 @@ class WordPressLoaderPlugin implements PluginInterface
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new LibraryInstaller($io, $composer, 'wordpress-loader');
+        var_dump( $installer );
         $composer->getInstallationManager()->addInstaller($installer);
     }
 }
