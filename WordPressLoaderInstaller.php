b8424829900f665f6705809f2ea02471ec43d028
Ján Bočínec <jan.bocinec@webikon.sk>
2016-07-08 21:08:47 +0200

    final testing

---
+++ WordPressLoaderInstaller.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Webikon\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class WordPressLoaderInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+
+        if ($this->composer->getPackage()) {
+            $extra = $this->composer->getPackage()->getExtra();
+            if (!empty($extra['installer-paths'])) {
+                $customPath = $this->mapCustomInstallPaths($extra['installer-paths'], $prettyName, $type, $vendor);
+                if ($customPath !== false) {
+                    return $this->templatePath($customPath, $availableVars);
+                }
+            }
+        }
+
+        return 'wordpress/wp-content/mu-plugins/';
+    }
+
+    /**
+     * Replace vars in a path
+     *
+     * @param  string $path
+     * @param  array  $vars
+     * @return string
+     */
+    protected function templatePath($path, array $vars = array())
+    {
+        if (strpos($path, '{') !== false) {
+            extract($vars);
+            preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
+            if (!empty($matches[1])) {
+                foreach ($matches[1] as $var) {
+                    $path = str_replace('{$' . $var . '}', $$var, $path);
+                }
+            }
+        }
+        return $path;
+    }
+    
+    /**
+     * Search through a passed paths array for a custom install path.
+     *
+     * @param  array  $paths
+     * @param  string $name
+     * @param  string $type
+     * @param  string $vendor = NULL
+     * @return string
+     */
+    protected function mapCustomInstallPaths(array $paths, $name, $type, $vendor = NULL)
+    {
+        foreach ($paths as $path => $names) {
+            if (in_array($name, $names) || in_array('type:' . $type, $names) || in_array('vendor:' . $vendor, $names)) {
+                return $path;
+            }
+        }
+        return false;
+    }
+}
